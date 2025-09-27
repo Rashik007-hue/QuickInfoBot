76ea1f8ccf6839614a46689b7a0a6b7db3993900
Rashik007-hue <rashikkhangm5@gmail.com>
2025-09-27 11:33:48 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) @TheSmartBisnu
-# Channel: https://t.me/itsSmartDev
-
-# Pyrogram setup
-API_ID = "26178555"  # Replace this API ID with your actual API ID
-API_HASH = "efbc8b9c52746de10a4808b229845f65"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "8158795669:AAGV1B4GepA7xXs44TUqoUKbGP1LPcQhAbk"  # Replace this BOT_TOKEN
